f98268a60a6a0e1ad3165b75122bf16cefd96982
Jendrick Burgos <147339@swcsd.us>
2025-05-09 08:15:41 -0400

    Initial Commit

---
+++ Potota/PototaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PototaApp.swift
+//  Potota
+//
+//  Created by student 01 on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PototaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PototaTests/PototaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PototaTests.swift
+//  PototaTests
+//
+//  Created by student 01 on 5/9/25.
+//
+
+import Testing
+@testable import Potota
+
+struct PototaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
